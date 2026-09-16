1b445cb9fbf94222dcedde499fef55bd71850973
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2026-09-16 16:59:19 +0400

    Django 14

--- Django 14/Django_14_FastAPI_books/routers/auth_router.py
+++ Django 14/Django_14_FastAPI_books/routers/auth_router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from starlette import status
 
@@ -32,17 +33,32 @@ async def register(
     return user
 
 
+# @router.post(
+#     "/login",
+#     response_model=TokenOut,
+#     status_code=status.HTTP_200_OK,
+# )
+# async def login(
+#         payload: UserLogin,
+#         db: Session = Depends(get_db),
+# ):
+#     user = db.query(User).filter(User.email == payload.email).first()
+#     if not user or not verify_password(payload.password, user.password_hash):
+#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email or password")
+#     token = create_access_token(sub=user.email, role=user.role.value)
+#     return {"access_token": token, "token_type": "bearer"}
+
+
 @router.post(
     "/login",
-    response_model=TokenOut,
     status_code=status.HTTP_200_OK,
 )
 async def login(
-        payload: UserLogin,
+        form_data: OAuth2PasswordRequestForm = Depends(),
         db: Session = Depends(get_db),
 ):
-    user = db.query(User).filter(User.email == payload.email).first()
-    if not user or not verify_password(payload.password, user.password_hash):
+    user = db.query(User).filter(User.email == form_data.username).first()
+    if not user or not verify_password(form_data.password, user.password_hash):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email or password")
     token = create_access_token(sub=user.email, role=user.role.value)
     return {"access_token": token, "token_type": "bearer"}
